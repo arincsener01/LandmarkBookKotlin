0269c45f7a6ee8d16e4429c9b54c4b0b7953ccac
Arinc Sener <senerar-nc@windowslive.com>
2023-12-03 14:54:19 +0300

    Landmark list is added

---
+++ app/src/main/java/com/arincdogansener/landmarkbookkotlin/Landmark.kt
@@ -0,0 +1,4 @@
+package com.arincdogansener.landmarkbookkotlin
+
+class Landmark (val name : String, val country : String, val image : Int){
+}
--- app/src/main/java/com/arincdogansener/landmarkbookkotlin/MainActivity.kt
+++ app/src/main/java/com/arincdogansener/landmarkbookkotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package com.arincdogansener.landmarkbookkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arincdogansener.landmarkbookkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var landmarkList: ArrayList<Landmark>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        landmarkList = ArrayList<Landmark>()
+
+        val pisa = Landmark("Pisa", "Italy", R.drawable.pisa)
+        val colosseum = Landmark("Colosseum", "Italy", R.drawable.colosseum)
+        val eiffel = Landmark("Eiffel", "France", R.drawable.eiffel)
+        val londonBridge = Landmark("London Bridge", "England", R.drawable.londonbridge)
+
+        landmarkList.add(pisa)
+        landmarkList.add(colosseum)
+        landmarkList.add(eiffel)
+        landmarkList.add(londonBridge)
+
+        
     }
 }
